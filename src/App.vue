9be119f1bc8599d933977802427ce117eae5afde
Jorge Isaza <isaza.jorge@hotmail.com>
2021-09-13 20:52:38 -0500

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+</template>
+
+<script lang="ts" setup>
+import HelloWorld from "./components/HelloWorld.vue";
+</script>
+
+<style>
+#app {
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
